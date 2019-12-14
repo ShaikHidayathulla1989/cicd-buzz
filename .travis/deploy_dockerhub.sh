814fe3540482f722e07da5511b5a895e4cea66ef
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 21:55:28 +0530

    Added echo comments

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,9 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
+echo "Docker Username $DOCKER_USER"
+echo "Docker Pass $DOCKER_PASS"
+echo "Docker Registry $DOCKER_REGISTRY"
 docker build -f Dockerfile -t "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" .
-docket tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" 
-docker push ${DOCKER_USER}/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
+docker tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" 
+docker push "${DOCKER_USER}/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
