39d63c45cd59a464824d761b90b6cf545bf92755
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 22:02:18 +0530

    Missed quotes

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -8,6 +8,9 @@ fi
 echo "Docker Username $DOCKER_USER"
 echo "Docker Pass $DOCKER_PASS"
 echo "Docker Registry $DOCKER_REGISTRY"
+
 docker build -f Dockerfile -t "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" .
-docker tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" 
-docker push "${DOCKER_USER}/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
+
+docker tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
+ 
+docker push "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
