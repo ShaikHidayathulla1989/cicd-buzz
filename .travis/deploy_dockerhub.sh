ba0d657ffae49da345dc35f4d6182b79f99e82d8
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 21:37:47 +0530

    Added docker username to docker build

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t ${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}:$TAG .
+docker build -f Dockerfile -t $DOCKER_USER/{TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}:$TAG .
 docker push $DOCKER_USER/${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}:$TAG
