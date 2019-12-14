63fa1c8d033327c6883670890c8666b8ca35d8ab
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 22:35:26 +0530

    Removed braces

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}" "${DOCKER_REGISTRY}"
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" "$DOCKER_REGISTRY"
 
-DOCKER_USERNAME="${DOCKER_USER}"
+DOCKER_USERNAME="$DOCKER_USER"
 
-echo "Docker Username $DOCKER_USERNAME"
+echo "Docker Username "$DOCKER_USERNAME"
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo "Docker User ${DOCKER_USER}"
-echo "Docker Pass ${DOCKER_PASS}"
-echo "Docker Registry ${DOCKER_REGISTRY}"
+echo "Docker User $DOCKER_USER"
+echo "Docker Pass $DOCKER_PASS"
+echo "Docker Registry $DOCKER_REGISTRY"
 
 docker build -f Dockerfile -t "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" .
 
-docker tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
+docker tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" "$DOCKER_USER"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
  
-docker push "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
+docker push "$DOCKER_USER"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
