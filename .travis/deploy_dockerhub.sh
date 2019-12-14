635275d32ff182b745d0836864fa89b7464eefb1
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 22:13:33 +0530

    Added braces to echo comments

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}" "${DOCKER_REGISTRY}"
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo "Docker Username $DOCKER_USER"
-echo "Docker Pass $DOCKER_PASS"
-echo "Docker Registry $DOCKER_REGISTRY"
+echo "Docker Username ${DOCKER_USER}"
+echo "Docker Pass ${DOCKER_PASS}"
+echo "Docker Registry ${DOCKER_REGISTRY}"
 
 docker build -f Dockerfile -t "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" .
 
