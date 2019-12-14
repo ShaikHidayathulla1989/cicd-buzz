5b1a60570eed30918da2ace8188369b5e6c28066
Shaik Hidayathulla <shaikhidayathulla@Shaiks-MacBook-Pro.local>
2019-12-14 21:49:57 +0530

    Added tag

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}:$TAG .
-docker push $DOCKER_USER/${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}:$TAG
+docker build -f Dockerfile -t "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" .
+docket tag "${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}" "${DOCKER_USER}"/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG" 
+docker push ${DOCKER_USER}/"${TRAVIS_BRANCH}"_"${TRAVIS_BUILD_NUMBER}":"$TAG"
